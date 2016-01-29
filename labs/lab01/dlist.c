fc820738204bb48d3a90f82553d9505004cfb104
Dunni Adenuga <ia005@bucknell.edu>
2016-01-29 16:17:30 -0500

    Problem 1 done

---
+++ labs/lab01/dlist.c
@@ -0,0 +1,145 @@
+/*
+ * Implementation of 'dlist,' a simple doubly linked list.
+ * This version of the program has memory leak issues
+ * and segmentation fault issues. Students are supposed 
+ * to fix these problems with the tools of 'gdb' and
+ * 'Valgrind'.
+ * 
+ * Xiannong Meng
+ * 2015-12-07
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "dlist.h"
+
+/*
+ * Creates an empty list.
+ *
+ * Parameters: none.
+ * Return: created list with head == tail == null.
+ */
+struct dlist * dlist_create() {
+
+  struct dlist * new_list;
+
+  new_list = (struct dlist *)malloc(sizeof(struct dlist));
+  new_list->head = NULL;
+  new_list->tail = NULL;
+
+  return new_list;
+}
+
+/*
+ * Insert a node into the list.
+ * The node and the list have been created.
+ *
+ * Parameters:
+ *   a_node:   the node to be inserted.
+ *   the_list: the doubly linked to which the new node will be added.
+ */
+void dlist_insert(struct dnode * a_node, struct dlist * the_list) {
+
+  if (dlist_is_empty(the_list) == true) { // insert at the begin
+    the_list->head = a_node;
+    the_list->tail = a_node;
+    a_node->next = NULL;
+    a_node->prev = NULL;
+  } else {                                // insert at the end
+    the_list->tail->next = a_node;
+    a_node->prev = the_list->tail;
+    a_node->next = NULL;
+    the_list->tail = a_node;
+  }
+}
+
+/*
+ * Remove a node with specific value.
+ *
+ * Parameters:
+ *   value_to_remove: specific value to remove
+ *   the_list: the doubly linked list from which the node will be removed.
+ */
+void dlist_remove(char * value_to_remove, struct dlist * the_list) {
+
+  struct dnode * node_to_remove;
+
+  if (dlist_is_empty(the_list) == false) {
+    node_to_remove = the_list->head;
+    while (node_to_remove != NULL
+	   && strcmp(node_to_remove->word, value_to_remove) != 0) {
+      node_to_remove = node_to_remove->next;
+    } // end of while
+
+    if (node_to_remove != NULL) {
+      // remove the node
+      //node_to_remove->next->prev = node_to_remove->prev;
+      if(node_to_remove->prev == NULL) {
+	the_list->head = node_to_remove->next;
+	node_to_remove->next->prev = NULL;
+	
+      }
+      if(node_to_remove->next == NULL) {
+	node_to_remove->prev->next = NULL;
+      }
+      if((node_to_remove->prev != NULL) && (node_to_remove->next != NULL)){
+
+	node_to_remove->prev->next = node_to_remove->next;
+      }
+    }
+
+  }  // end of list not empty
+}
+
+/*
+ * Test if a list is empty.
+ */
+bool dlist_is_empty(struct dlist * the_list) {
+  
+  return (the_list->head == NULL);
+}
+
+/*
+ * Traverse the list from begin to end, print all information.
+ *
+ * Parameters:
+ *    a_list: the list to traverse.
+ */
+void dlist_traverse(struct dlist * a_list) {
+
+  struct dnode * ptr;
+
+  if (a_list == NULL)
+    return;
+  else {
+    ptr = a_list->head;
+    while (ptr != NULL) {
+      printf("%d %s\n", ptr->count, ptr->word);
+      ptr = ptr->next;
+    } // end of while
+  }
+}
+
+/*
+ * make a new dnode with 'word' and 'count' as contents.
+ * initialize all links to be null.
+ *
+ * Parameters:
+ *   word: the word to be stored in the node.
+ *   count: the count value to be stored.
+ *
+ * Return: the newly created node with contents.
+ */
+struct dnode * make_node(char * word, int count) {
+
+  struct dnode * new_node = (struct dnode *)malloc(sizeof(struct dnode));
+
+  new_node->word = (char *)malloc(strlen(word) + 1);
+  strcpy(new_node->word, word);
+  new_node->count = count;
+  new_node->prev = new_node->next = NULL;
+
+  return new_node;
+}
---
+++ labs/lab01/test_dlist.c
@@ -0,0 +1,56 @@
+/*
+ * Drive program to test a doubly linked list.
+ *
+ * Xiannong Meng
+ * 2015-12-08
+ *
+ * To compile
+ *    make
+ * Sample run
+ *   ./test_dlist < /usr/share/dict/words
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "dlist.h"
+
+#define NUM_WORD 20      // number of words to test
+#define WORD_LEN 64      // word length
+
+int main(int argc, char* argv[]) {
+
+  struct dlist * a_list = dlist_create();
+  struct dnode * a_node;
+  int i;
+  int r;
+  char word[NUM_WORD][WORD_LEN];
+  
+  srandom((int)time(NULL));     // initalize random number generator
+
+  for (i = 0; i < NUM_WORD; i ++) {
+    scanf("%s", word[i]);
+    a_node = make_node(word[i], i);
+    dlist_insert(a_node, a_list);
+  }
+
+  printf("after inserting %d words...\n", NUM_WORD);
+  dlist_traverse(a_list);
+
+  r = random() % NUM_WORD;
+  printf("removing  node %d ...\n", r);
+  dlist_remove(word[r], a_list);
+
+  printf("removing node 0 - %dst node ...\n", 1);
+  
+  dlist_remove(word[0], a_list);
+   
+
+  printf("removing last node - the %dth node ...\n", NUM_WORD -1);
+  dlist_remove(word[NUM_WORD-1], a_list);
+
+  printf("after the removal...\n");
+  dlist_traverse(a_list);
+
+  return 0;
+}
